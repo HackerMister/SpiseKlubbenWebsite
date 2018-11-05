171550f4b5a2184baeecc09aa7b8997fb25b8264
HackerMister <43439996+HackerMister@users.noreply.github.com>
2018-11-05 17:09:24 +0100

    na

--- script/javascript.js
+++ script/javascript.js
@@ -1,16 +1,2 @@
 // javascript
 
-//navbar fixed when scrolling
-window.onscroll = function() {myFunction()};
-
-var stayPut = document.getElementById('navbar');
-
-var sticky = stayPut.offsetTop;
-
-function myFunction() {
-    if (window.pageYOffset > sticky) {
-        stayPut.classList.add('sticky');
-    } else {
-        stayPut.classList.remove('sticky');
-    }
-}
