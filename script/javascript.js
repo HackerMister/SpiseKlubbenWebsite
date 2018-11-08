188942d21d6ba5fc24ac311618d4006322c736da
HackerMister <43439996+HackerMister@users.noreply.github.com>
2018-11-08 17:21:37 +0100

    footer

--- script/javascript.js
+++ script/javascript.js
@@ -7,3 +7,5 @@
     var clone = document.importNode(template.content, true);
 
     document.querySelector('#putfooterhere').appendChild(clone);
+
+// code from https://www.html5rocks.com/en/tutorials/webcomponents/imports/
