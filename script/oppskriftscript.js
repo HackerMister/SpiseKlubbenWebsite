bebb984ce1b121e50d2f3a11cd21717bb13ae8c9
HackMasterNice <runepi@stud.ntnu.no>
2018-11-09 14:08:20 +0100

    fårdetikketil

--- script/oppskriftscript.js
+++ script/oppskriftscript.js
@@ -2,39 +2,61 @@
 // script for oppskrift-oversikt
 /*stolen from: https://www.w3schools.com/howto/howto_js_portfolio_filter.asp  MÅ UTSKIFTES!!*/
 
-filterSelection("all")
-function filterSelection(c) {
-    var x, i;
-    x = document.getElementsByClassName("category");
-    if (c == "all") c = "";
-    for (i = 0; i < x.length; i++) {
-        w3RemoveClass(x[i], "show");
-        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+//filterSelection("all")
+//function filterSelection(c) {
+//    var x, i;
+//    x = document.getElementsByClassName("category");
+//    if (c == "all") c = "";
+//    for (i = 0; i < x.length; i++) {
+//        w3RemoveClass(x[i], "show");
+//        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+//    }
+//}
+//
+//function w3AddClass(element, name) {
+//    var i, arr1, arr2;
+//    arr1 = element.className.split(" ");
+//    arr2 = name.split(" ");
+//    for (i = 0; i < arr2.length; i++) {
+//        if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i]; }
+//    }
+//}
+//
+//function w3RemoveClass(element, name) {
+//    var i, arr1, arr2;
+//    arr1 = element.className.split(" ");
+//    arr2 = name.split(" ");
+//    for (i = 0; i < arr2.length; i++) {
+//        while (arr1.indexOf(arr2[i]) > -1) {
+//            arr1.splice(arr1.indexOf(arr2[i]), 1);
+//        }
+//    }
+//    element.className = arr1.join(" ");
+//}
+
+let parent = document.getElementsByClassName('opp_oversikt_wrapper');
+let classnName = document.getElementsByClassName
+function showOnly(parent, className) {
+  className = ' ' + className + ' ';
+
+  var e = parent.firstChild;
+  while (e != null) {
+    if (e.nodeType == 1) {
+      if ((' ' + e.className + ' ').indexOf(className) > -1)
+        e.style.display = 'block';
+      else
+        e.style.display = 'none';
     }
+
+    e = e.nextSibling;
+  }
 }
 
-function w3AddClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-        if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i]; }
-    }
-}
+showOnly(document.getElementById('masterdiv'), 'cat02');
 
-function w3RemoveClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-        while (arr1.indexOf(arr2[i]) > -1) {
-            arr1.splice(arr1.indexOf(arr2[i]), 1);
-        }
-    }
-    element.className = arr1.join(" ");
-}
 
-// adds 'active' class to the selected category, 
+// adds 'active' class to the selected category,
+
 let buttons = document.querySelectorAll('.category_selector');
 console.log(buttons);
 
@@ -42,6 +64,7 @@ for (button of buttons) {
     button.addEventListener('click', function() {
         for (button of buttons) {
             button.classList.remove("active");
+            
         }
         this.classList.add('active');
 })
